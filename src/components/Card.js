bd494ee3bbc1c98b728e110ed7a12400f48b5b2c
Gloria Vela <gloria.laura.vela@gmail.com>
2019-02-20 15:01:22 -0600

    URL params and dynamic routing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card.css';
+
+// The most important aspect of routing is when you need to have dynamic routes and pass specific props related to this route.
+// For example, we click on any of those cards we would want to get to the actual page for that product.
 
 const Card = (props) =>  (
   <div className={props.card.animation}>
-    <div className="front">
-      <img src="juice.jpg" alt="Avatar" className="card-image" />
-      <div className="container">
-        <h3>{props.card.title}</h3>
-        <h3><span className="price"> ${props.card.price}</span></h3> 
-        <p>{props.card.description}</p>
+    <Link to={`/product/${props.card.id}`}>
+      <div className="front">
+        <img src="juice.jpg" alt="Avatar" className="card-image" />
+        <div className="container">
+          <h3>{props.card.title}</h3>
+          <h3><span className="price"> ${props.card.price}</span></h3> 
+          <p>{props.card.description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   </div>
 );
 
